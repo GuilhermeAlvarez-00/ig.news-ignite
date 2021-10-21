365a7a23ea2dd968d676fe7a3d781382d3cefb11
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2021-10-21 16:24:11 -0300

    redirecionando para pagina de checkout do stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,17 +1,41 @@
+import { AxiosPromise, AxiosRequestConfig } from 'axios'
 import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+interface PostProps {
+  sessionId: string
+}
+
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
   const [session] = useSession()
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session) {
       signIn()
       return
+    }
+
+    try {
+      const response = await api.post<PostProps>('subscribe')
+
+      let sessionId = null
+      const { data } = response
+      sessionId = data.sessionId
+      console.log(sessionId)
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({
+        sessionId
+      })
+    } catch(err) {
+      alert(err.message)
     }
   }
 
@@ -19,6 +43,7 @@ export function SubscribeButton({priceId}: SubscribeButtonProps) {
     <button
       type="button"
       className={styles.subcribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -23,7 +23,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL
     })
 
-    return res.status(200).json({ sessionId: stripeCheckoutSession.id})
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method not allowed')
