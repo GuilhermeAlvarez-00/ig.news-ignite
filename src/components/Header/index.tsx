01cb1a17b4b7837b53cf3acafee1dda495f2bb4d
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2021-11-20 00:13:54 -0300

    adicionando menu de navegação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -7,8 +8,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo ignews" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
         
         <SignInButton />
