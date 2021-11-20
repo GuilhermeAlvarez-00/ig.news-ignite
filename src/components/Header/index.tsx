d96b53032f231607204356280ace6fcc73a7c5d2
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2021-11-20 00:36:15 -0300

    configurando marcador da pagina

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -8,12 +8,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo ignews" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/" prefetch>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         
         <SignInButton />
