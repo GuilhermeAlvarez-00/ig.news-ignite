7cd58db2242ca2b6ece06b5d4372aa9a67e0c0f0
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2021-11-23 21:50:20 -0300

    configurando next auth para deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home({product}: HomeProps) {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access to all the publications <br/>
-            <span>for {product.amount} month!</span>
+            <span>for {product.amount} month</span>
           </p>
 
           <SubscribeButton priceId={product.priceId} />
