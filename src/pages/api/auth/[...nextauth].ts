220445ff2afae75cd9876fbd24d7058123f154cd
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-05-18 14:11:48 -0300

    atualizando price_id

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,13 @@
-import { query as q, query } from 'faunadb'
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
-import { fauna } from '../../../services/fauna'
+import { query as q, query } from 'faunadb';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user',
     }),
   ],
@@ -30,57 +30,43 @@ export default NextAuth({
                   )
                 )
               ),
-              q.Match(
-                q.Index('subscription_by_status'),
-                'active'
-              )
+              q.Match(q.Index('subscription_by_status'), 'active'),
             ])
           )
-        )
-  
+        );
+
         return {
           ...session,
-          activeSubscription: userActiveSubscription
-        }
+          activeSubscription: userActiveSubscription,
+        };
       } catch {
         return {
           ...session,
-          activeSubscription: null
-        }
+          activeSubscription: null,
+        };
       }
     },
 
     async signIn(user) {
-      const { email } = user
+      const { email } = user;
 
       try {
         await fauna.query(
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              {data: { email }}
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
-            )
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
           )
-        )
-  
-        return true
+        );
+
+        return true;
       } catch {
-        return false
+        return false;
       }
     },
-  }
-})
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,32 @@
-import { GetStaticProps } from 'next'
-import Head from 'next/head'
-import { SubscribeButton } from '../components/SubscribeButton'
-import { stripe } from '../services/stripe'
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-import styles from './home.module.scss'
+import styles from './home.module.scss';
 
 interface HomeProps {
   product: {
     priceId: string;
     amount: number;
-  }
+  };
 }
 
-export default function Home({product}: HomeProps) {
-
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
-          <p>Get access to all the publications <br/>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
 
@@ -34,24 +36,24 @@ export default function Home({product}: HomeProps) {
         <img src="/images/avatar.svg" alt="Image of a girl codding" />
       </main>
     </>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1JwUTXLfUcFNO6F9sxu0Lwft')
+  const price = await stripe.prices.retrieve('price_1Kyf8kJ8PUEGsIQ2OwdvmTrI');
 
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD'
-    }).format(Number(price.unit_amount) / 100)
-  }
+      currency: 'USD',
+    }).format(Number(price.unit_amount) / 100),
+  };
 
   return {
     props: {
-      product
+      product,
     },
-    revalidate: 60 * 60 * 24 // 24 hours
-  }
-}
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
