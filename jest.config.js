251dd4775af7177bf57c701efe0c202dcd1c49fd
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-05-19 15:36:00 -0300

    feat: adicionando e configurando jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
